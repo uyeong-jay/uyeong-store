177011bc67452b392c50ca44f08688f19535456c
william-jacob <wjacob2103@gmail.com>
2021-11-08 18:29:53 +0900

    [UPDATE] updated bootstrap ver

--- pages/_document.js
+++ pages/_document.js
@@ -11,10 +11,9 @@ class MyDocument extends Document{
           <meta name="desciption" content="Sell-photos website with Next.js" />
 
           {/* 부트스트랩 ver 4 - CSS, JS*/}
-          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />
-          <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" ></script>
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" ></script>
-          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" ></script>
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" />
+          <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
 
           {/* Font Awesome 5 Intro */}
           <script src="https://kit.fontawesome.com/a076d05399.js"></script>
