29afc48a916d1b977ba0aef5a0d54d82d02994c1
william-jacob <wjacob2103@gmail.com>
2021-11-08 18:31:02 +0900

    [ADD && UPDATE] added user model

---
+++ models/userModel.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  name: {
+      type: String,
+      required: true
+  },
+  email: {
+      type: String,
+      required: true,
+      unique: true
+  },
+  password: {
+      type: String,
+      required: true
+  },
+  role: {
+      type: String,
+      default: 'user'
+  },
+  root: {
+      type: Boolean,
+      default: false
+  },
+  avatar: {
+    type: String,
+    //클라우디너리
+    default: 'https://res.cloudinary.com/devatchannel/image/upload/v1602752402/avatar/avatar_cugq40.png'
+  }
+}, {
+  timestamps: true
+});
+
+let Dataset = mongoose.models.user || mongoose.model('user', userSchema);
+export default Dataset;
