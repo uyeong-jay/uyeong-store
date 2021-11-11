b2bb4fabb6b1381dc734b6a532db217e7f73fd1a
william-jacob <wjacob2103@gmail.com>
2021-11-11 10:59:34 +0900

    #Login [UPDATE] added localstorage and cookies for removal

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Link from 'next/link';
+import Cookies from 'js-cookie';
 import { useRouter } from 'next/router';
+import { DataContext } from '../store/globalState';
+import { TYPES } from '../store/types';
 
 const NavBar = () => {
   const router = useRouter();
   //pathname(string): 현재경로
-  const isActive = rt => rt === router.pathname ? " active" :  ""
+  const isActive = rt => rt === router.pathname ? " active" :  "";
+
+  const {state, dispatch} = useContext(DataContext);
+  const { auth } = state;
+
+
+  const onClickLogout = () => {
+    Cookies.remove('refreshtoken', { path: 'api/auth/accessToken' });
+    localStorage.removeItem('firstLogin');
+    dispatch({ type: TYPES.AUTH, payload: {} });
+    dispatch({ type: TYPES.NOTIFY, payload: { success: 'Logged out!' } })
+  };
 
   return (
     // 부트스트랩 ver 4 - navbar
@@ -14,6 +28,7 @@ const NavBar = () => {
         <a className="navbar-brand">UYeong-Mall</a>
       </Link>
 
+      {/* Toggle-Button*/}
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -21,6 +36,8 @@ const NavBar = () => {
       <div className="collapse navbar-collapse" id="navbarNavDropdown" style={{ justifyContent: 'end' }}>
 
         <ul className="navbar-nav">
+
+          {/* Cart */}
           {/* isActive(): " active" or "" */}
           <li className={"nav-item" + isActive('/cart')}>
             <Link href="/cart">
@@ -29,27 +46,35 @@ const NavBar = () => {
             </Link>
           </li>
 
-          <li className={"nav-item" + isActive('/signin')}>
-            <Link href="/signin">
-              {/* font awesome - user (+ aria-hidden )*/}
-              <a className="nav-link"><i className="fas fa-user" aria-hidden="true"></i> Sign in</a>
-            </Link>
-          </li>
+          {
+            //Object.keys(obj): obj의 key들만 모아서 배열로 반환
+            Object.keys(auth).length === 0 //인증 정보 유무
+            ? (
+              // Sign-in 
+              <li className={"nav-item" + isActive('/signin')}>
+                <Link href="/signin">
+                  {/* font awesome - user (+ aria-hidden )*/}
+                  <a className="nav-link"><i className="fas fa-user" aria-hidden="true"></i> Sign in</a>
+                </Link>
+              </li>
+            ) : (
+              // Dropdown-menu
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <img src={auth.user.avatar} alt={auth.user.avatar} 
+                  style={{ width: '20px', height: '20px', marginRight: '4px', marginBottom: '3px'}} />
+                  {auth.user.name}
+                </a>
+                <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <a className="dropdown-item" href="#">Profile</a>
+                  <button className="dropdown-item" onClick={onClickLogout}>Logout</button>
+                </div>
+              </li>
+            )
+          }
 
-          {/* <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              User Name
-            </a>
-
-            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <a className="dropdown-item" href="#">Profile</a>
-              <a className="dropdown-item" href="#">Logout</a>
-            </div>
-          </li> */}
         </ul>
-        
       </div>
-
     </nav>
   );
 };
