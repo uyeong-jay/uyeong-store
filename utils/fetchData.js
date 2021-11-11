698ef8d0f4aa68485ee726109d7099ab1c24a71e
william-jacob <wjacob2103@gmail.com>
2021-11-11 11:02:09 +0900

    [UPDATE] added comment

--- utils/fetchData.js
+++ utils/fetchData.js
@@ -1,14 +1,17 @@
 const baseUrl = process.env.BASE_URL;
 
+//GET
 export const getData = async (url, token) => {
   const res = await fetch(`${baseUrl}/api/${url}`, {
     method: 'GET',
     headers: { 'Authorization': token }
   });
   const data = await res.json();
-  return data;
+  return data; // api폴더로 부터 받은 데이터
 };
 
+
+//POST
 export const postData = async (url, post, token) => {
   const res = await fetch(`${baseUrl}/api/${url}`, {
     method: 'POST',
@@ -19,9 +22,11 @@ export const postData = async (url, post, token) => {
     body: JSON.stringify(post)
   });
   const data = await res.json();
-  return data; //api로 부터 받은 성공, 실패 데이터
+  return data;
 };
 
+
+//PUT
 export const putData = async (url, post, token) => {
   const res = await fetch(`${baseUrl}/api/${url}`, {
     method: 'PUT',
@@ -35,6 +40,8 @@ export const putData = async (url, post, token) => {
   return data;
 };
 
+
+//PATCH
 export const patchData = async (url, post, token) => {
   const res = await fetch(`${baseUrl}/api/${url}`, {
     method: 'PATCH',
@@ -48,6 +55,8 @@ export const patchData = async (url, post, token) => {
   return data;
 };
 
+
+//DELETE
 export const deleteData = async (url, token) => {
   const res = await fetch(`${baseUrl}/api/${url}`, {
     method: 'DELETE',
