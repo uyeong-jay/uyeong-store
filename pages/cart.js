c9da2a5cab1402c7b988414005c8c7aab83ec3e8
william-jacob <wjacob2103@gmail.com>
2021-11-12 15:46:48 +0900

    #Cart [UPDATE] add empty cart image

--- pages/cart.js
+++ pages/cart.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Head from 'next/head';
+import { DataContext } from '../store/globalState';
 
-const cart = () => {
+const Cart = () => {
+  const { state, dispatch } = useContext(DataContext);
+  const { cart } = state;
+
+
+
+  
   return (
-    <div>
-      cart
-    </div>
+    <>
+      <Head>
+        <title>Cart</title>
+      </Head>
+      {
+        cart.length <= 0 
+        ? (<img className="d-block mx-auto img-responsive w-50 my-5" src="/empty_cart.png" alt="empty_cart" />)
+        : (<h2>Cart</h2>)
+      }
+    </>
   );
 };
 
-export default cart;
+export default Cart;
