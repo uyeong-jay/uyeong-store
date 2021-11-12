9413c669ab68a1327c2c0eeea8a26fec31188cd3
william-jacob <wjacob2103@gmail.com>
2021-11-12 15:41:50 +0900

    #Cart [UPDATE] added localstorage for cart

--- store/globalState.js
+++ store/globalState.js
@@ -9,19 +9,20 @@ export const DataContext = createContext();
 
 //provider 생성
 export const DataProvider = ({ children }) => {
-  const initialState = { notify: {}, auth: {} };
+  const initialState = { notify: {}, auth: {}, cart: [] };
 
   //useReducer
   const [state, dispatch] = useReducer(reducers, initialState);
+  const { cart } = state;
 
-  
-  //firstLogin이 존재할시: token, user정보를 auth에 다시 넣어주기
+  //-------------localStorage 정보 유지-------------
+  //스토리지에 firstLogin이 존재할시: auth정보 재dispatch > 새로고침시에도 유저 정보 유지
   useEffect(() => {
     const firstLogin = localStorage.getItem("firstLogin");
     
     if (firstLogin) {
-      getData('auth/accessToken').then((res) => {
 
+      getData('auth/accessToken').then((res) => {
         if (res.err) return localStorage.removeItem("firstLogin");
         return dispatch({ type: TYPES.AUTH, payload: {
           token: res.access_token,
@@ -32,6 +33,17 @@ export const DataProvider = ({ children }) => {
     }
   },[])
 
+  //스토리지에 user_cart가 존재할시: cart정보 재dispatch >  새로고침시에도 유저 cart 정보 유지
+  useEffect(() => {
+    const user_cart = JSON.parse(localStorage.getItem('user_cart')); //user_cart: [ product:{제품정보}, ... ]
+
+    if (user_cart) return dispatch({ type: TYPES.ADD_CART, payload: user_cart });
+  },[]);
+  //초기 user_cart 생성 후, cart에 변화있을 때마다 스토리지에 바뀐 cart 저장 
+  useEffect(() => {
+    localStorage.setItem('user_cart', JSON.stringify(cart));
+  },[cart]);
+
 
   return (
     //context + provider
@@ -40,3 +52,8 @@ export const DataProvider = ({ children }) => {
     </DataContext.Provider>
   );
 };
+
+//상태 데이터 구조
+// notify: { loading: "", success: "", error: "" }
+// auth: { user: {유저정보}, token: "" }
+// cart: [ product: {제품정보}, ... ]
