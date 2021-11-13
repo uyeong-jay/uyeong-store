ed9f42c5a10dc4763f7b7d46947d8b49a70a6c25
william-jacob <wjacob2103@gmail.com>
2021-11-13 12:46:31 +0900

    #Cart [UPDATE] added  cart form

--- pages/cart.js
+++ pages/cart.js
@@ -1,11 +1,12 @@
 import React, { useContext } from 'react';
 import Head from 'next/head';
+import Link from 'next/link';
 import { DataContext } from '../store/globalState';
 import CartItem from '../components/cart/CartItem';
 
 const Cart = () => {
   const { state, dispatch } = useContext(DataContext);
-  const { cart } = state;
+  const { auth, cart } = state;
 
 
 
@@ -17,29 +18,35 @@ const Cart = () => {
       </Head>
       {
         cart.length <= 0 
-        // image-empty_cart
+        // empty_cart-image
         ? (<img className="d-block mx-auto img-responsive w-50 my-5" src="/empty_cart.png" alt="empty_cart" />)
         : (
           <>
+            {/* cart-table */}
             <div className="col-md-8 table-responsive my-3 text-secondary">
-              {/* cart-title */}
               <h2 className="text-uppercase">Shopping Cart</h2>
-              {/* cart-table */}
+
               <table className="table my-3">
                 <tbody>{cart.map((item) => (<CartItem key={item._id} item={item} cart={cart} dispatch={dispatch} />))}</tbody>
               </table>
             </div>
 
-            <div className="col-md-4 text-secondary">
+
+            {/* cart-shopping-form */}
+            <div className="col-md-4 text-right text-uppercase text-secondary">
               <form>
                 <h2>Shopping</h2>
-
-                <label>Address</label>
-                <input />
-
-                <label>Mobile</label>
-                <input />
+                <label htmlFor="address">Address</label>
+                <input type="text" name="address" id="address" className="form-control mb-2" />
+                <label htmlFor="mobile">Mobile</label>
+                <input type="text" name="mobile" id="mobile" className="form-control mb-2" />
               </form>
+
+              <h3>Total: <span className="text-info">$0</span></h3>
+
+              <Link href={auth.user ? '#' : '/signin'}>
+                <a className="btn btn-dark my-2 text-capitalize">Proceed with payment</a>
+              </Link>
             </div>
           </>
         )
