de0b488d3e4786adf28e81fd01f8d5ab7831f0b9
william-jacob <wjacob2103@gmail.com>
2021-11-13 12:47:19 +0900

    #Cart [UPDATE] added counting cart quantity

--- components/cart/CartItem.js
+++ components/cart/CartItem.js
@@ -10,9 +10,9 @@ const CartItem = ({ item, cart, dispatch }) => {
         style={{ minWidth: '80px', height: '80px' }}/>
       </td>
 
-      {/* cart-item-title */}
+      {/* cart-item-info */}
       <td style={{ minWidth: '200px' }}>
-        <h5 className="text-capitalize text-secondary my-2">
+        <h5 className="text-capitalize text-secondary">
           <Link href={`/product/${item._id}`}><a>{item.title}</a></Link>
         </h5>
         <h6 className="text-danger">${item.quantity * item.price}</h6>
@@ -22,6 +22,20 @@ const CartItem = ({ item, cart, dispatch }) => {
           : (<p className="text-danger">Out Stock</p>)
         }
       </td>
+
+      {/* cart-count-quantity */}
+      <td className="align-middle" style={{ minWidth: '150px' }}>
+        <button type="button" className="btn btn-outline-secondary" style={{ width:'30px', height: '30px', padding: 0 }}>-</button>
+        <span className="mx-2">{item.quantity}</span>
+        <button type="button" className="btn btn-outline-secondary" style={{ width:'30px', height: '30px', padding: 0 }}>+</button>
+      </td>
+      
+      {/* cart-delete-icon */}
+      <td className="align-middle" style={{ minWidth: '50px', cursor: 'pointer' }}>
+        {/* font awesome - delete (+ aria-hidden ) */}
+        <i className="far fa-trash-alt text-danger" aria-hidden='true'></i>
+      </td>
+
     </tr>
   );
 };
