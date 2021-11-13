29717adb8bb2c954533a49e57a3a5b3f85f23a94
william-jacob <wjacob2103@gmail.com>
2021-11-13 11:34:53 +0900

    #Cart [UPDATE] added cartItem compoenent with map

--- pages/cart.js
+++ pages/cart.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import Head from 'next/head';
 import { DataContext } from '../store/globalState';
+import CartItem from '../components/cart/CartItem';
 
 const Cart = () => {
   const { state, dispatch } = useContext(DataContext);
@@ -10,16 +11,40 @@ const Cart = () => {
 
   
   return (
-    <>
+    <div className="row mx-auto">
       <Head>
         <title>Cart</title>
       </Head>
       {
         cart.length <= 0 
+        // image-empty_cart
         ? (<img className="d-block mx-auto img-responsive w-50 my-5" src="/empty_cart.png" alt="empty_cart" />)
-        : (<h2>Cart</h2>)
+        : (
+          <>
+            <div className="col-md-8 table-responsive my-3 text-secondary">
+              {/* cart-title */}
+              <h2 className="text-uppercase">Shopping Cart</h2>
+              {/* cart-table */}
+              <table className="table my-3">
+                <tbody>{cart.map((item) => (<CartItem key={item._id} item={item} cart={cart} dispatch={dispatch} />))}</tbody>
+              </table>
+            </div>
+
+            <div className="col-md-4 text-secondary">
+              <form>
+                <h2>Shopping</h2>
+
+                <label>Address</label>
+                <input />
+
+                <label>Mobile</label>
+                <input />
+              </form>
+            </div>
+          </>
+        )
       }
-    </>
+    </div>
   );
 };
 
