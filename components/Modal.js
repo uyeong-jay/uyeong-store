505a03f4234f46f9a121601a183ce0737ced6c5a
william-jacob <wjacob2103@gmail.com>
2021-11-15 10:31:24 +0900

    #Modal [UPDATE] added modal dispatch

--- components/Modal.js
+++ components/Modal.js
@@ -1,36 +1,41 @@
 import React, { useContext } from 'react';
 import { DataContext } from '../store/globalState';
 import { deleteItem } from '../store/actions';
+import { TYPES } from '../store/types';
 
 const Modal = () => {
   const { state, dispatch } = useContext(DataContext);
-  const { modal } = state;
+  const { modal } = state; // modal: { data: [], id: "", title: "" }
 
-  const onClickButton = () => {};
+  const onClickDelete = () => {
+    dispatch(deleteItem(modal.data, modal.id, TYPES.ADD_CART)); //modal.data(cart전체)중 일치id 외 나머지만 cart에 남겨두기
+    dispatch({ type: TYPES.ADD_MODAL, payload: {} }); //modal 초기화
+  };
 
 
   return (
     // bootstrap 4 modal - live demo
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
+    <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div className="modal-dialog" role="document">
+        <div className="modal-content">
 
-          <div class="modal-header">
-            {/* modal-title */}
-            <h5 class="modal-title" id="exampleModalLabel">{modal.title}</h5>
-            {/* modal-close-button */}
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          {/* modal-header */}
+          <div className="modal-header">
+            <h5 className="modal-title text-capitalize" id="exampleModalLabel">{modal.title}</h5>
+            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
 
-          <div class="modal-body">
+          {/* modal-body */}
+          <div className="modal-body">
             Do you really want to delete this item?
           </div>
 
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal" onClick={onClickButton}>Yes</button>
-            <button type="button" class="btn btn-primary" data-dismiss="modal">Cancel</button>
+          {/* modal-footer (+ data-dismiss="modal")*/}
+          <div className="modal-footer">
+            <button type="button" className="btn btn-secondary" data-dismiss="modal" onClick={onClickDelete}>Yes</button>
+            <button type="button" className="btn btn-primary" data-dismiss="modal">Cancel</button>
           </div>
 
         </div>
