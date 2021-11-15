35e6a575fb99b13d56e92dd41c137aa68841e695
william-jacob <wjacob2103@gmail.com>
2021-11-15 20:20:59 +0900

    #Order [UPDATE] imported postData function

--- components/PaypalBtn.js
+++ components/PaypalBtn.js
@@ -1,40 +1,67 @@
 import React, { useEffect, useRef } from "react";
+import { postData } from "../utils/fetchData";
+import { TYPES } from "../store/types";
 
 const PaypalBtn = ({ totalPrice, address, mobile, state, dispatch }) => {
   const refPayPalBtn = useRef();
   const { cart, auth } = state;
 
   useEffect(() => {
-    paypal.Buttons({
+    paypal
+      .Buttons({
+        createOrder: function (data, actions) {
+          // This function sets up the details of the transaction, including the amount and line item details.
+          return actions.order.create({
+            purchase_units: [
+              {
+                amount: {
+                  value: totalPrice,
+                },
+              },
+            ],
+          });
+        },
 
-      createOrder: function(data, actions) {
-        // This function sets up the details of the transaction, including the amount and line item details.
-        return actions.order.create({
-          purchase_units: [{
-            amount: {
-              value: totalPrice
-            }
-          }]
-        });
-      },
+        onApprove: function (data, actions) {
+          // This function captures the funds from the transaction.
 
-      onApprove: function(data, actions) {
-        // This function captures the funds from the transaction.
-        return actions.order.capture().then(function(details) {
-          // This function shows a transaction success message to your buyer.
-          console.log(data);
-          alert('Transaction completed by ' + details.payer.name.given_name);
-        });
-      }
-      
-    }).render(refPayPalBtn.current);
+          //결제 완료 전 로딩화면
+          dispatch({ type: TYPES.NOTIFY, payload: { loading: true } });
+
+          //결제 완료 후 실행 함수
+          return actions.order.capture().then(function (details) {
+            // This function shows a transaction success message to your buyer.
+
+            //결제 후 응답 받기
+            postData(
+              "order",
+              { address, mobile, cart, totalPrice },
+              auth.token
+            ).then((res) => {
+              //console.log(res); >> { msg: "", newOrder: {user, address, mobile, cart, totalPrice} }
+
+              if (res.err)
+                return dispatch({
+                  type: TYPES.NOTIFY,
+                  payload: { error: res.err },
+                }); //에러 메세지
+
+              dispatch({ type: TYPES.ADD_CART, payload: [] }); //카트 초기화
+              return dispatch({
+                type: TYPES.NOTIFY,
+                payload: { success: res.msg },
+              }); //성공 메세지
+            });
+
+            alert("Transaction completed by " + details.payer.name.given_name);
+          });
+        },
+      })
+      .render(refPayPalBtn.current);
     //This function displays Smart Payment Buttons on your web page.
-  }, [])
+  }, []);
 
-
-  return (
-    <div ref={refPayPalBtn}></div>
-  );
+  return <div ref={refPayPalBtn}></div>;
 };
 
 export default PaypalBtn;
