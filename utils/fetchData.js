fa6209432aa65ac1057b6b9c91f29ce661dad672
william-jacob <wjacob2103@gmail.com>
2021-11-17 19:59:45 +0900

    [UPDATE] apply prettier

--- utils/fetchData.js
+++ utils/fetchData.js
@@ -3,66 +3,62 @@ const baseUrl = process.env.BASE_URL;
 //GET
 export const getData = async (url, token) => {
   const res = await fetch(`${baseUrl}/api/${url}`, {
-    method: 'GET',
-    headers: { 'Authorization': token }
+    method: "GET",
+    headers: { Authorization: token },
   });
   const data = await res.json();
   return data; // api폴더로 부터 받은 데이터
 };
 
-
 //POST
 export const postData = async (url, post, token) => {
   const res = await fetch(`${baseUrl}/api/${url}`, {
-    method: 'POST',
-    headers: { 
-      'Content-Type': 'application/json',
-      'Authorization': token 
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token,
     },
-    body: JSON.stringify(post)
+    body: JSON.stringify(post),
   });
   const data = await res.json();
   return data;
 };
-
 
 //PUT
 export const putData = async (url, post, token) => {
   const res = await fetch(`${baseUrl}/api/${url}`, {
-    method: 'PUT',
-    headers: { 
-      'Content-Type': 'application/json',
-      'Authorization': token 
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token,
     },
-    body: JSON.stringify(post)
+    body: JSON.stringify(post),
   });
   const data = await res.json();
   return data;
 };
-
 
 //PATCH
 export const patchData = async (url, post, token) => {
   const res = await fetch(`${baseUrl}/api/${url}`, {
-    method: 'PATCH',
-    headers: { 
-      'Content-Type': 'application/json',
-      'Authorization': token 
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token,
     },
-    body: JSON.stringify(post)
+    body: JSON.stringify(post),
   });
   const data = await res.json();
   return data;
 };
 
-
 //DELETE
 export const deleteData = async (url, token) => {
   const res = await fetch(`${baseUrl}/api/${url}`, {
-    method: 'DELETE',
-    headers: { 
-      'Content-Type': 'application/json',
-      'Authorization': token 
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token,
     },
   });
   const data = await res.json();
