54111a25ce724d21c79b31826d9107e3743110df
william-jacob <wjacob2103@gmail.com>
2021-11-19 16:57:20 +0900

    #Order [UPDATE] added orders state

--- store/globalState.js
+++ store/globalState.js
@@ -1,52 +1,69 @@
 //contxet 생성 , useReducer 생성
-import { createContext, useReducer, useEffect } from 'react';
-import reducers from './reducers';
-import { getData } from '../utils/fetchData';
-import { TYPES } from './types';
+import { createContext, useReducer, useEffect } from "react";
+import reducers from "./reducers";
+import { getData } from "../utils/fetchData";
+import { TYPES } from "./types";
 
 //context 담기
 export const DataContext = createContext();
 
 //provider 생성
 export const DataProvider = ({ children }) => {
-  const initialState = { notify: {}, auth: {}, cart: [], modal: {} };
+  const initialState = {
+    notify: {},
+    auth: {},
+    cart: [],
+    modal: {},
+    orders: [],
+  };
 
   //useReducer
   const [state, dispatch] = useReducer(reducers, initialState);
-  const { cart } = state;
+  const { cart, auth } = state;
 
   //-------------localStorage 정보 유지-------------
-  //스토리지에 firstLogin이 존재할시: auth정보 재dispatch > 새로고침시에도 유저 정보 유지
+  //스토리지에 firstLogin이 존재할시: auth정보 다시dispatch >> 새로고침시에도 유저 정보 유지
   useEffect(() => {
     const firstLogin = localStorage.getItem("firstLogin");
-    
+
     if (firstLogin) {
-
-      getData('auth/accessToken').then((res) => {
+      getData("auth/accessToken").then((res) => {
         if (res.err) return localStorage.removeItem("firstLogin");
-        return dispatch({ type: TYPES.AUTH, payload: {
-          token: res.access_token,
-          user: res.user
-        }});
-
+        return dispatch({
+          type: TYPES.AUTH,
+          payload: {
+            token: res.access_token,
+            user: res.user,
+          },
+        });
       });
     }
-  },[])
+  }, []);
 
-  //스토리지에 user_cart가 존재할시: cart정보 재dispatch >  새로고침시에도 유저 cart 정보 유지
+  //스토리지에 user_cart가 존재할시: cart정보 다시dispatch >> 새로고침시에도 유저 cart 정보 유지
   useEffect(() => {
-    const user_cart = JSON.parse(localStorage.getItem('user_cart')); //user_cart: [ product:{제품정보}, ... ]
+    const user_cart = JSON.parse(localStorage.getItem("user_cart")); //user_cart: [ product:{제품정보}, ... ]
 
-    if (user_cart) return dispatch({ type: TYPES.ADD_CART, payload: user_cart });
-  },[]);
-  //초기 user_cart 생성 후, cart에 변화있을 때마다 스토리지에 바뀐 cart 저장 
+    if (user_cart)
+      return dispatch({ type: TYPES.ADD_CART, payload: user_cart });
+  }, []);
+
+  //초기 user_cart 생성 후, cart에 변화있을 때마다 스토리지에 바뀐 cart 저장
   useEffect(() => {
-    localStorage.setItem('user_cart', JSON.stringify(cart));
-  },[cart]);
+    localStorage.setItem("user_cart", JSON.stringify(cart));
+  }, [cart]);
 
+  //token이 다시 넣어질때 마다 주문 했던 목록 상태 업데이트
+  useEffect(() => {
+    if (auth.token) {
+      getData("order", auth.token).then((res) => {
+        if (res.err)
+          return dispatch({ type: TYPES.NOTIFY, payload: { error: res.err } });
 
-
-
+        return dispatch({ type: TYPES.ADD_ORDERS, payload: res.orders }); //orders: 주문된 목록(배열)
+      });
+    }
+  }, [auth.token]);
 
   return (
     //context + provider
--- store/reducers.js
+++ store/reducers.js
@@ -1,32 +1,36 @@
 import { TYPES } from "./types";
 
-
 //state: { notify: {}, auth: {}, cart: [], modal: {} }
 const reducers = (state, action) => {
   switch (action.type) {
     case TYPES.NOTIFY:
       return {
         ...state,
-        notify: action.payload
+        notify: action.payload,
       };
     case TYPES.AUTH:
       return {
         ...state,
-        auth: action.payload
+        auth: action.payload,
       };
     case TYPES.ADD_CART:
       return {
         ...state,
-        cart: action.payload
+        cart: action.payload,
       };
     case TYPES.ADD_MODAL:
       return {
         ...state,
-        modal: action.payload
+        modal: action.payload,
+      };
+    case TYPES.ADD_ORDERS:
+      return {
+        ...state,
+        orders: action.payload,
       };
     default:
       return state;
-  };
+  }
 };
 
 export default reducers;
