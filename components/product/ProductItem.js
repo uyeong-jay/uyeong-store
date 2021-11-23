30eb0e59559b4ca0816606d68c5ba8e474842a07
william-jacob <wjacob2103@gmail.com>
2021-11-23 17:30:08 +0900

    [UPDATE] apply prettier

--- components/product/ProductItem.js
+++ components/product/ProductItem.js
@@ -1,64 +1,74 @@
-import React, { useContext } from 'react';
-import Link from 'next/link';
-import { DataContext } from '../../store/globalState';
-import { addToCart } from '../../store/actions';
+import React, { useContext } from "react";
+import Link from "next/link";
+import { DataContext } from "../../store/globalState";
+import { addToCart } from "../../store/actions";
+import { TYPES } from "../../store/types";
 
 const ProductItem = ({ product }) => {
   const { state, dispatch } = useContext(DataContext);
   const { cart } = state; //[ product:{제품정보}, ... ]
 
+  const onClickCart = () => {
+    dispatch(addToCart(product, cart));
+    return dispatch({
+      type: TYPES.NOTIFY,
+      payload: { success: "added to cart" },
+    });
+  };
 
   const userLink = () => {
     return (
       <>
         {/* View */}
         <Link href={`/product/${product._id}`}>
-          <a className="btn btn-info" style={{marginRight: '5px', flex: 1}}>View</a>
+          <a className="btn btn-info" style={{ marginRight: "5px", flex: 1 }}>
+            View
+          </a>
         </Link>
         {/* Buy */}
-        <button className="btn btn-success" 
-        style={{ marginLeft: '5px', flex: 1 }} 
-        disabled={product.inStock === 0 ? true : false}
-        onClick={() => dispatch(addToCart(product, cart))}
-        >Buy</button>
+        <button
+          className="btn btn-success"
+          style={{ marginLeft: "5px", flex: 1 }}
+          disabled={product.inStock === 0 ? true : false}
+          onClick={onClickCart}
+        >
+          Cart
+        </button>
       </>
     );
   };
 
-
   return (
     // 부트스트랩 4 - card
     // class, style >> className, style={{}}
-    <div className="card" style={{ width: '18rem' }}>
-
+    <div className="card" style={{ width: "18rem" }}>
       {/* image */}
-      <img className="card-img-top" src={product.images[0].url} alt={product.images[0].url} />
+      <img
+        className="card-img-top"
+        src={product.images[0].url}
+        alt={product.images[0].url}
+      />
 
       <div className="card-body">
-
         {/* title (+text-capitalize: 첫글자만 대문자로)*/}
         <h5 className="card-title text-capitalize">{product.title}</h5>
 
         {/* price && stock (+ 양쪽으로 배치) */}
         <div className="row justify-content-between mx-0">
           <h6 className="text-danger">${product.price}</h6>
-          {
-            product.inStock > 0 
-            ? (<h6 className="text-danger">In Stock: {product.inStock}</h6>)
-            : (<h6 className="text-danger">Out Stock</h6>)
-          }
+          {product.inStock > 0 ? (
+            <h6 className="text-danger">In Stock: {product.inStock}</h6>
+          ) : (
+            <h6 className="text-danger">Out Stock</h6>
+          )}
         </div>
 
         {/* description */}
-        <p className="card-text">{product.description.substr(0, 80) + '...'}</p>
+        <p className="card-text">{product.description.substr(0, 80) + "..."}</p>
 
         {/* button - View, Buy (+ 양쪽으로 배치) */}
-        <div className="row justify-content-between mx-0">
-          {userLink()}
-        </div>
-
+        <div className="row justify-content-between mx-0">{userLink()}</div>
       </div>
-
     </div>
   );
 };
@@ -76,6 +86,5 @@ const ProductItem = ({ product }) => {
 //       description: "~"
 //     },
 //     ...
-
 
 export default ProductItem;
--- store/actions.js
+++ store/actions.js
@@ -1,34 +1,50 @@
 import { TYPES } from "./types";
 
-
-export const addToCart = (product, cart) => { //product: 제품하나{}, cart: 제품들[]
+export const addToCart = (product, cart) => {
+  //product: 제품하나{}, cart: 제품들[]
   //살려는 제품의 재고가 없을때 에러 메세지
   if (product.inStock <= 0) {
-    return ({ type: TYPES.NOTIFY, payload: { error: 'This product is out of stock.' } });
+    return {
+      type: TYPES.NOTIFY,
+      payload: { error: "This product is out of stock." },
+    };
   }
 
-  //cart안에 하나라도 같은 제품이 없어야 ture 반환
+  //cart안에 하나라도 같은 제품이 없어야 true 반환
   const checkCart = cart.every((item) => item._id !== product._id);
-  if (!checkCart) return ({ type: TYPES.NOTIFY, payload: { error: 'The product has been added to cart.' } });
+  if (!checkCart) {
+    return {
+      type: TYPES.NOTIFY,
+      payload: { error: "The product has been added to cart." },
+    };
+  }
 
   //cart에 product추가(+ product에 quantity 추가)
-  return ({ type: TYPES.ADD_CART, payload : [...cart, { ...product, quantity: 1 }] }); // ...틀이름 
+  return {
+    type: TYPES.ADD_CART,
+    payload: [...cart, { ...product, quantity: 1 }],
+  };
 };
-
-
 
 //count quantity
 export const count = (cart, productId, sign) => {
   const newCart = [...cart];
-  if (sign === "-") newCart.filter((item) => item._id === productId)[0].quantity -= 1;
-  if (sign === "+") newCart.filter((item) => item._id === productId)[0].quantity += 1;
-  return ({ type: TYPES.ADD_CART, payload: newCart });
+  if (sign === "-")
+    newCart.filter((item) => item._id === productId)[0].quantity -= 1;
+  if (sign === "+")
+    newCart.filter((item) => item._id === productId)[0].quantity += 1;
+  return { type: TYPES.ADD_CART, payload: newCart };
 };
-
-
 
 //delete item
 export const deleteItem = (data, productId, type) => {
   const newData = data.filter((item) => item._id !== productId); //인자로 들어온 item 외 나머지를 반환
-  return ({ type, payload: newData });
+  return { type, payload: newData };
+};
+
+//update item
+export const updateItem = (data, productId, post, type) => {
+  const newData = data.map((item) => (item._id === productId ? post : item)); //post: {paid: true, dateOfpayment: "", paymentId: "", method: "Paypal"} 반환
+
+  return { type, payload: newData };
 };
