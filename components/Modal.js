4f8a7b9e6ade6d904aaa6b998e835e0a7b85ab88
william-jacob <wjacob2103@gmail.com>
2021-11-28 00:54:19 +0900

    #Modal [UPDATE] added deleteData and contents to modal

--- components/Modal.js
+++ components/Modal.js
@@ -1,16 +1,32 @@
-import React, { useContext } from "react";
+import React, { useContext, useMemo } from "react";
 import { DataContext } from "../store/globalState";
 import { deleteItem } from "../store/actions";
 import { TYPES } from "../store/types";
+import { deleteData } from "../utils/fetchData";
 
 const Modal = () => {
   const { state, dispatch } = useContext(DataContext);
-  const { modal } = state; // modal: { data: [], id: "", title: "" }
+  const { auth, modal } = state; // modal: { data: [], id: "", title: "" }
 
   const onClickDelete = () => {
+    if (modal.type === TYPES.ADD_USERS) {
+      deleteData(`user/${modal.id}`, auth.token).then((res) => {
+        if (res.err)
+          return dispatch({ type: TYPES.NOTIFY, payload: { error: res.err } }); //에러
+        return dispatch({ type: TYPES.NOTIFY, payload: { success: res.msg } }); //성공
+      });
+    }
     dispatch(deleteItem(modal.data, modal.id, modal.type)); //modal.data(cart전체)중 일치id 외 나머지만 cart에 남겨두기
     return dispatch({ type: TYPES.ADD_MODAL, payload: {} }); //modal 초기화
   };
+
+  //modal 내용
+  const modalbodyMsg = useMemo(() => {
+    if (modal.type === TYPES.ADD_CART)
+      return "Do you want to delete this item?";
+    if (modal.type === TYPES.ADD_USERS)
+      return "Do you want to delete this user?";
+  }, [modal.type]);
 
   return (
     // bootstrap 4 modal - live demo
@@ -40,7 +56,7 @@ const Modal = () => {
           </div>
 
           {/* modal-body */}
-          <div className="modal-body">Do you want to delete this item?</div>
+          <div className="modal-body">{modalbodyMsg}</div>
 
           {/* modal-footer (+ data-dismiss="modal")*/}
           <div className="modal-footer">
