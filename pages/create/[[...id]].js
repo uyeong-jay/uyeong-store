ddcc989e270f610919500a21c5db76f49ade05c8
william-jacob <wjacob2103@gmail.com>
2021-12-01 13:50:25 +0900

    #Create [UPDATE] updated upload function

--- pages/create/[[...id]].js
+++ pages/create/[[...id]].js
@@ -1,9 +1,12 @@
 import React, { useContext, useState } from "react";
 import Head from "next/head";
+import { useRouter } from "next/router";
 import { DataContext } from "../../store/globalState";
 import { TYPES } from "../../store/types";
+import { imageUpload } from "../../utils/imageUpload";
+import { putData, postData } from "../../utils/fetchData";
 
-//[[...id]]: Catch all routes - /create, /create/a, /create/a/b
+//[[...id]]: Catch all routes - /create, /create/id1, /create/id1/id2
 const ProductsManager = () => {
   const initialState = {
     title: "",
@@ -21,6 +24,11 @@ const ProductsManager = () => {
   const { auth, categories } = state;
 
   const [images, setImages] = useState([]);
+  const [imgCount, setImgCount] = useState(0);
+  const [onEdit, setOnEdit] = useState(false);
+
+  const router = useRouter();
+  const { id } = router.query;
 
   //input 데이터 저장
   const onChangeInput = (e) => {
@@ -31,40 +39,108 @@ const ProductsManager = () => {
 
   //이미지 업로드
   const onChangeUpload = (e) => {
-    dispatch({ type: TYPES.NOTIFY, payload: {} });
-
     let selectedImages = [];
-    let num = 0;
     let err = "";
 
-    //선택한 파일들
-    const files = [...e.currentTarget.files];
+    dispatch({ type: TYPES.NOTIFY, payload: {} }); //메세지 초기화
 
-    //파일 유무 에러
+    //선택한 파일들
+    const files = [...e.currentTarget.files]; //배열 >> forEach
+
+    //파일 선택 에러
     if (files.length <= 0)
       return dispatch({
         type: TYPES.NOTIFY,
-        payload: { error: "No files selected!." },
+        payload: { error: "No files selected!" },
       });
 
     files.forEach((file) => {
-      //파일 사이즈 에러
-      if (file.size > 1024 * 1024)
-        return (err = "The image size must be less than 1MB.");
+      //파일 크기 에러
+      if (file.size > 400 * 300)
+        return (err = "The image size must be less than 1KB.");
 
-      //파일 확장자 에러
+      //파일 확장자 에러 (jpg, png O)
       if (file.type !== "image/jpg" && file.type !== "image/png")
         return (err = "Image format is incorrect.");
 
-      //파일 선택 갯수 제한
-      num += 1;
-      if (num <= 5) selectedImages.push(file);
-      else return (err = "You can upload up to 5 images.");
+      //파일 갯수 제한
+      if (imgCount < 5) {
+        setImgCount((imgCount += 1));
+        selectedImages.push(file);
+      } else {
+        return (err = "You can upload up to 5 images.");
+      }
 
       return selectedImages;
     });
 
-    if (err) dispatch({ type: TYPES.NOTIFY, payload: { error: err } });
+    if (err) dispatch({ type: TYPES.NOTIFY, payload: { error: err } }); //에러
+    setImages([...images, ...selectedImages]); //이미지 저장
+  };
+
+  //파일 삭제
+  const onClickDelete = (index) => {
+    images.splice(index, 1);
+    setImgCount((imgCount -= 1));
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    //백으로 가기전에 미리 검사
+    if (auth.user.role !== "admin")
+      return dispatch({
+        type: TYPES.NOTIFY,
+        payload: { error: "Authentication is not valid." },
+      });
+
+    //백으로 가기전에 미리 검사
+    if (
+      !title ||
+      !price ||
+      !inStock ||
+      !description ||
+      !content ||
+      category === "all" ||
+      images.length <= 0
+    )
+      return dispatch({
+        type: TYPES.NOTIFY,
+        payload: { error: "Please add all the fields." },
+      });
+
+    dispatch({ type: "NOTIFY", payload: { loading: true } }); //로딩
+
+    let media = []; // [ {public_id: "" url: ""}, ... ]
+    const imgNewURL = images.filter((img) => !img.url); //media배열에 존재하지 않는 image(객체)(url-x)
+    const imgOldURL = images.filter((img) => img.url); //media배열에 이미 존재하는 imgae(객체)(url-o)
+    // console.log(typeof imgNewURL, typeof imgOldURL); //object, object
+
+    //imgNewURL(객체)존재시 imageUpload 유틸에 imgNewURL(객체) 전달
+    if (imgNewURL.length > 0) media = await imageUpload(imgNewURL);
+    // >> cloud에 저장된후, 반환값을 media에 저장 by imageUpload
+    // console.log(media); // [ {public_id: "" url: ""}, ... ]
+
+    let res; //res를 활용하기 위해 fetch대신 async/await 사용
+    if (onEdit) {
+      res = await putData(
+        `product/${id}`,
+        { ...product, images: [...imgOldURL, ...media] },
+        auth.token
+      );
+      if (res.err)
+        return dispatch({ type: "NOTIFY", payload: { error: res.err } });
+    } else {
+      res = await postData(
+        "product",
+        { ...product, images: [...imgOldURL, ...media] },
+        auth.token
+      );
+      if (res.err)
+        return dispatch({ type: "NOTIFY", payload: { error: res.err } });
+    }
+
+    return dispatch({ type: "NOTIFY", payload: { success: res.msg } });
   };
 
   return (
@@ -73,7 +149,7 @@ const ProductsManager = () => {
         <title>Products Manager</title>
       </Head>
 
-      <form className="row">
+      <form className="row container" onSubmit={onSubmit}>
         <div className="col-md-6">
           {/* title */}
           <input
@@ -90,10 +166,10 @@ const ProductsManager = () => {
             {/* price */}
             <div className="col-sm-6">
               <input
-                className="d-block w-100 p-2"
+                className="d-block w-100 mb-4 p-2"
                 type="number"
                 name="price"
-                value={title}
+                value={price}
                 onChange={onChangeInput}
                 placeholder="Price"
               />
@@ -102,10 +178,10 @@ const ProductsManager = () => {
             {/* inStock */}
             <div className="col-sm-6">
               <input
-                className="d-block w-100 p-2"
+                className="d-block w-100 mb-4 p-2"
                 type="number"
                 name="inStock"
-                value={title}
+                value={inStock}
                 onChange={onChangeInput}
                 placeholder="Stock"
               />
@@ -114,13 +190,14 @@ const ProductsManager = () => {
 
           {/* description */}
           <textarea
-            className="d-block w-100 my-4 p-2"
+            className="d-block w-100 p-2"
             name="description"
             id="description"
             cols="30"
             rows="4"
-            placeholder="Description"
+            value={description}
             onChange={onChangeInput}
+            placeholder="Description"
           />
 
           {/* content */}
@@ -130,8 +207,9 @@ const ProductsManager = () => {
             id="content"
             cols="30"
             rows="6"
-            placeholder="Content"
+            value={content}
             onChange={onChangeInput}
+            placeholder="Content"
           />
 
           {/* category (+ bootstrap 4 - custom select) */}
@@ -178,19 +256,33 @@ const ProductsManager = () => {
             </div>
 
             {/* images */}
-            <div className="row">
-              {images.map((image) => (
-                <div>
+            <div className="product_images row mx-auto my-2">
+              {images.map((image, index) => (
+                <div className="porduct_image mb-2" key={index}>
+                  {/* image */}
                   <img
-                    className="img-thumbnail rounded-lg"
+                    className="img-thumbnail border-info"
                     src={image.url ? image.url : URL.createObjectURL(image)}
                     onLoad={() => URL.revokeObjectURL(image)}
                     alt={image.url}
                   />
+
+                  {/* button - delete */}
+                  <button type="button" onClick={() => onClickDelete(index)}>
+                    X
+                  </button>
                 </div>
               ))}
             </div>
           </div>
+
+          {/* button - update, create */}
+          <button
+            className="float-right btn btn-primary mb-4 px-4"
+            type="submit"
+          >
+            Create
+          </button>
         </div>
       </form>
     </div>
