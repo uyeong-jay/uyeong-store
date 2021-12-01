721a37536cde06fb5454a4c1f9a1b46c6b690a5b
william-jacob <wjacob2103@gmail.com>
2021-12-01 13:42:34 +0900

    #Create [UPDATE] added functions to update and create

--- pages/api/product/[id].js
+++ pages/api/product/[id].js
@@ -1,28 +1,70 @@
-import connectDB from '../../../utils/connectDB';
-import Products from '../../../models/productModel';
+import connectDB from "../../../utils/connectDB";
+import Products from "../../../models/productModel";
+import auth from "../../../middleware/auth";
 
 connectDB();
 
 export default async (req, res) => {
-  switch(req.method){
+  switch (req.method) {
     case "GET":
       await getProduct(req, res);
       break;
-  };
+    case "PUT":
+      await updateProduct(req, res);
+      break;
+  }
 };
-
 
 const getProduct = async (req, res) => {
   try {
     const { id } = req.query;
 
     const product = await Products.findById(id);
-    if(!product) return res.status(400).json({ err: 'This product does not exist.' });
-    
-    res.json({ product });
+    if (!product)
+      return res.status(400).json({ err: "This product does not exist." });
 
+    res.json({ product });
   } catch (err) {
-      return res.status(500).json({ err: err.message });
+    return res.status(500).json({ err: err.message });
   }
 };
 
+const updateProduct = async (req, res) => {
+  try {
+    const result = await auth(req, res);
+    if (result.role !== "admin")
+      return res.status(400).json({ err: "Authentication is not valid." });
+
+    const { id } = req.query;
+    const { title, price, inStock, description, content, category, images } =
+      req.body;
+
+    if (
+      !title ||
+      !price ||
+      !inStock ||
+      !description ||
+      !content ||
+      category === "all" ||
+      images.length === 0
+    )
+      return res.status(400).json({ err: "Please add all the fields." });
+
+    await Products.findOneAndUpdate(
+      { _id: id },
+      {
+        title: title.toLowerCase(),
+        price,
+        inStock,
+        description,
+        content,
+        category,
+        images,
+      }
+    );
+
+    res.json({ msg: "Update Success!" });
+  } catch (err) {
+    return res.status(500).json({ err: err.message });
+  }
+};
--- pages/api/product/index.js
+++ pages/api/product/index.js
@@ -1,29 +1,68 @@
-import connectDB from '../../../utils/connectDB';
-import Products from '../../../models/productModel';
+import connectDB from "../../../utils/connectDB";
+import Products from "../../../models/productModel";
+import auth from "../../../middleware/auth";
 
 connectDB();
 
 export default async (req, res) => {
-  switch(req.method){
+  switch (req.method) {
     case "GET":
       await getProducts(req, res);
       break;
-  };
+    case "Post":
+      await createProduct(req, res);
+      break;
+  }
 };
-
 
 const getProducts = async (req, res) => {
   try {
     const products = await Products.find();
-    
-    res.json({
-        status: 'success',
-        result: products.length,
-        products
-    });
 
+    res.json({
+      status: "success",
+      result: products.length,
+      products,
+    });
   } catch (err) {
     return res.status(500).json({ err: err.message });
   }
 };
 
+const createProduct = async (req, res) => {
+  try {
+    const result = await auth(req, res);
+    if (result.role !== "admin")
+      return res.status(400).json({ err: "Authentication is not valid." });
+
+    const { title, price, inStock, description, content, category, images } =
+      req.body;
+
+    if (
+      !title ||
+      !price ||
+      !inStock ||
+      !description ||
+      !content ||
+      category === "all" ||
+      images.length <= 0
+    )
+      return res.status(400).json({ err: "Please add all the fields." });
+
+    const newProduct = new Products({
+      title: title.toLowerCase(),
+      price,
+      inStock,
+      description,
+      content,
+      category,
+      images,
+    });
+
+    await newProduct.save();
+
+    res.json({ msg: "Create Success!" });
+  } catch (err) {
+    return res.status(500).json({ err: err.message });
+  }
+};
