7319822de1de317a6a1e8d64b0d41c7fa796382b
william-jacob <wjacob2103@gmail.com>
2022-02-24 11:11:57 +0900

    [UPDATE] added loading container

--- components/Loading.js
+++ components/Loading.js
@@ -1,12 +1,15 @@
 const Loading = () => {
   return (
     // Credit: https://dribbble.com/shots/5092176-Newton-Loader
-    <div className="gooey">
-      <span className="dot"></span>
-      <div className="dots">
-        <span></span>
-        <span></span>
-        <span></span>
+
+    <div className="container-loading">
+      <div className="gooey">
+        <span className="dot"></span>
+        <div className="dots">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
     </div>
   );
